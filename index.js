2e204c84d3e59541b83b0f18377facecf66429ff
mburrow303 <elio21@comcast.net>
2024-02-29 16:32:19 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,24 @@
+require("dotenv").config();
+
+const express = require("express");
+const app = express();
+
+const mongoose = require("mongoose");
+const { PORT, MONGO } = process.env;
+mongoose.connect(`${process.env.MONGO}/Capstone`);
+const db = mongoose.connection;
+db.once("open", () => console.log(`Connected to: ${MONGO}`));
+
+//const homeFeed =  require("./Controllers/home-feed.controller");
+const post = require("./Controllers/post.controller");
+const profile = require("./Controllers/profile.controller");
+//const search = require("./Controllers/search.controller"); //? STRETCH GOAL
+
+app.use(express.json());
+app.use(require("cors")());
+//app.use("/home", homeFeed);
+app.use("/post", post);
+app.use("/profile", profile);
+//app.use("/search", search); //? STRETCH GOAL
+
+app.listen(PORT, () => console.log(`App is listening on port ${PORT}`));
